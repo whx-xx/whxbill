106b2fcdbda03f435f71d5f453e527f2f1a4ea87
whx-xx <3243916556@qq.com>
2026-06-15 10:39:23 +0800

    chore: remove docs and deploy from tracking

--- deploy/frontend-user-dist/assets/fileUrl-B1ltmw2p.js
+++
@@ -1,1 +0,0 @@
-import{S as n}from"./index-C1eZr_qb.js";function o(t){return t?/^https?:\/\//i.test(t)||t.startsWith("data:")?t:`${n}/api/files/content?fileUrl=${encodeURIComponent(t)}`:""}export{o as b};
